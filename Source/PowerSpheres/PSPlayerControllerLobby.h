90a59d05f79def53654c50b0168eabcd56ca2751
Alberto Plaza <guibrush001@gmail.com>
2019-08-01 23:28:50 +0100

    Code passed from HUD blueprint to PlayerControllers

---
+++ Source/PowerSpheres/PSPlayerControllerLobby.h
@@ -0,0 +1,17 @@
+// Copyright 2019-2020 Alberto & co. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PSPlayerControllerLobby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POWERSPHERES_API APSPlayerControllerLobby : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PowerSpheres/Player/PSPlayerControllerMenu.h
@@ -0,0 +1,17 @@
+// Copyright 2019-2020 Alberto & co. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PSPlayerControllerMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POWERSPHERES_API APSPlayerControllerMenu : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
