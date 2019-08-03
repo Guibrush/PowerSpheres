4de26d0fa1476f11f2403bcbf9ace044cdf152a7
Alberto Plaza <guibrush001@gmail.com>
2019-08-04 00:29:19 +0100

    Added class to make the calculation of the damage.

--- Source/PowerSpheres/GAS/PSDamageExecCalc.cpp
+++ Source/PowerSpheres/GAS/PSDamageExecCalc.cpp
@@ -3,3 +3,19 @@
 
 #include "PSDamageExecCalc.h"
 
+UPSDamageExecCalc::UPSDamageExecCalc(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	FAttrCaptureDef Attributes;
+
+	RelevantAttributesToCapture.Add(Attributes.HealthDef);
+	RelevantAttributesToCapture.Add(Attributes.AttackDef);
+	RelevantAttributesToCapture.Add(Attributes.DefenceDef);
+
+	InvalidScopedModifierAttributes.Add(Attributes.HealthDef);
+}
+
+void UPSDamageExecCalc::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	
+}
--- Source/PowerSpheres/GAS/PSDamageExecCalc.h
+++ Source/PowerSpheres/GAS/PSDamageExecCalc.h
@@ -4,7 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "GameplayEffectExecutionCalculation.h"
+#include "PSUnitAttributeSet.h"
 #include "PSDamageExecCalc.generated.h"
+
+USTRUCT()
+struct POWERSPHERES_API FAttrCaptureDef
+{
+	GENERATED_USTRUCT_BODY()
+
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Health);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Attack);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Defence);
+
+	FAttrCaptureDef()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UPSUnitAttributeSet, Health, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UPSUnitAttributeSet, Attack, Source, true);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UPSUnitAttributeSet, Defence, Target, false);
+	}
+};
 
 /**
  * 
@@ -12,6 +30,10 @@
 UCLASS()
 class POWERSPHERES_API UPSDamageExecCalc : public UGameplayEffectExecutionCalculation
 {
-	GENERATED_BODY()
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const;
 	
 };
