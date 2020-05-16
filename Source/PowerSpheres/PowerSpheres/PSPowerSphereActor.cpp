93865de2886098ef6be9dfd9568bd4381be4973c
JaviGM <javi.galvanm@gmail.com>
2020-05-16 12:55:32 +0100

    Created PowerSphereActor C++

---
+++ Source/PowerSpheres/PowerSpheres/PSPowerSphereActor.cpp
@@ -0,0 +1,27 @@
+// Copyright 2019-2020 Alberto & co. All Rights Reserved.
+
+
+#include "PSPowerSphereActor.h"
+
+// Sets default values
+APSPowerSphereActor::APSPowerSphereActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APSPowerSphereActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APSPowerSphereActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
